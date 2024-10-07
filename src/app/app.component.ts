cc4476ecca5e1e986ff6cb48d9dfb1be20c5938d
santhan <santhankrishna18@gmail.com>
2024-10-07 13:54:50 +0530

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, NavbarComponent,RouterModule,RouterLink],
+  imports: [RouterOutlet, FormsModule, CommonModule, NavbarComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent},
-    {path:'contact',component:ContactComponent}
+    {path:'', component:HomeComponent},
+    {path:'contact', component:ContactComponent}
 ];
