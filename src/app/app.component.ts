faa5e0e6e67bd3b2e52298612ea222867db67dc4
santhan <santhankrishna18@gmail.com>
2024-10-07 09:31:11 +0530

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sthotras';
+  slides: string[] = [
+    'assets/1.jpg',
+    'assets/2.jpg',
+    'assets/3.jpg',
+    'assets/4.jpg',
+    'assets/5.jpg',
+    'assets/6.jpg',
+    'assets/7.jpg',
+    'assets/8.jpg',
+    'assets/9.jpg',
+    'assets/10.jpg',
+    'assets/11.jpg',
+    'assets/12.jpg',
+  ];
+
+  currentSlide = 0;
+
+  changeSlide(direction: number) {
+    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
+  }
 }
