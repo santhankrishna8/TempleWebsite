b12d625b1eb7ae3be3d7576bb1d81a4e46562da7
santhan <santhankrishna18@gmail.com>
2024-10-07 12:48:55 +0530

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, NavbarComponent,RouterModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sthotras';
-  slides: string[] = [
-    'assets/1.jpg',
-    'assets/2.jpg',
-    'assets/3.jpg',
-    'assets/4.jpg',
-    'assets/5.jpg',
-    'assets/6.jpg',
-    'assets/7.jpg',
-    'assets/8.jpg',
-    'assets/9.jpg',
-    'assets/10.jpg',
-    'assets/11.jpg',
-    'assets/12.jpg',
-  ];
-
-  currentSlide = 0;
-
-  changeSlide(direction: number) {
-    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
-  }
+  
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+declare const bootstrap: any;
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  slides: string[] = [
+    'assets/1.jpg',
+    'assets/2.jpg',
+    'assets/3.jpg',
+    'assets/4.jpg',
+    'assets/5.jpg',
+    'assets/6.jpg',
+    'assets/7.jpg',
+    'assets/8.jpg',
+    'assets/9.jpg',
+    'assets/10.jpg',
+    'assets/11.jpg',
+    'assets/12.jpg',
+  ];
+
+  currentSlide = 0;
+
+  changeSlide(direction: number) {
+    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
+  }
+  openModal() {
+    // Use Bootstrap's JavaScript to show the modal
+    const modalElement = document.getElementById('simpleModal');
+    const modal = new bootstrap.Modal(modalElement!);
+    modal.show();
+  }
+
+}
