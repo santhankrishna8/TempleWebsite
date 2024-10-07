19e55dacf030343762fa9be579755f9ea9a349e0
santhan <santhankrishna18@gmail.com>
2024-10-07 14:38:15 +0530

    all

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ declare const bootstrap: any;
 })
 export class HomeComponent {
   slides: string[] = [
-    'assets/1.jpg',
+    'assets/11.jpg',
     'assets/2.jpg',
     'assets/3.jpg',
     'assets/4.jpg',
@@ -21,7 +21,7 @@ export class HomeComponent {
     'assets/8.jpg',
     'assets/9.jpg',
     'assets/10.jpg',
-    'assets/11.jpg',
+    'assets/1.jpg',
     'assets/12.jpg',
   ];
 
